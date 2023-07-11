fd72d3b59b89429dd456459b528984bbba6e2525
村山一輝 <murayama@murayamaikkinoMacBook-Air.local>
2023-07-12 04:12:49 +0900

    main

--- src/js/db.js
+++ src/js/db.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCDkfr8oPTKVlycFuWe6P99oqREV__ZHAw",
-  authDomain: "kinos-597de.firebaseapp.com",
-  projectId: "kinos-597de",
-  storageBucket: "kinos-597de.appspot.com",
-  messagingSenderId: "58533156324",
-  appId: "1:58533156324:web:679af19f0908ce494063f1",
-  measurementId: "G-TZL7T5BTZS"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
   };
   
 
